16e660de51d08db718e8b0397246263de647e86a
Jelil Faisal Abudu <abudujelilfaisal@gmail.com>
2020-10-30 12:42:47 +0000

    Add home module

---
+++ src/modules/home.js
@@ -0,0 +1,16 @@
+import  '../assets/scss/home.scss';
+
+const homeContent = () => {
+  // const body = document.querySelector('body');
+  const contentDiv = document.getElementById('content');
+  const homeCaptionDiv = document.createElement('div');
+  const homeCaptionEl = document.createElement('p');
+  let homeCaption = 'have you tried our nutritious and delicious food varieties yet? visit us today!';
+
+  contentDiv.appendChild(homeCaptionDiv);
+  homeCaptionDiv.classList.add('home-caption-container');
+  homeCaptionDiv.appendChild(homeCaptionEl);
+  homeCaptionEl.textContent = homeCaption;
+};
+
+export default homeContent;
